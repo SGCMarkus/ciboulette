d7143249fa9acdd8ef22ca00e62da24e145de8b2
SGCMarkus <markusornik@gmail.com>
2023-07-02 14:45:27 +0200

    fix setup.py for proper module installation

--- setup.py
+++ setup.py
@@ -4,19 +4,27 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ciboulette-pkg-Touzan-Dominique", # Replace with your own username
-    version="0.0.1",
+    name="ciboulette-pkg-SGCMarkus", # Replace with your own username
+    version="0.1.0",
     author="Touzan Dominique",
-    author_email="dtouzan@gmail.com",
+    author_email="markusornik@gmail.com",
     description="Ciboulette package for astronomy",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dtouzan/ciboulette",
-    packages=setuptools.find_packages(),
+    url="https://github.com/SGCMarkus/ciboulette",
+    package_dir = {#"ciboulette": ".",
+                   "ciboulette.aavso": "aavso",
+                   "ciboulette.base": "base",
+                   "ciboulette.filtering": "filtering",
+                   "ciboulette.indiclient": "indiclient",
+                   "ciboulette.phd2client": "phd2client",
+                   "ciboulette.spectrum": "spectrum",
+                   "ciboulette.utils": "utils",
+                   "ciboulette.sector": "sector"},
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.9',
 )
